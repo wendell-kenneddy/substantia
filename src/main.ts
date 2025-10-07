97e46285818f4a8ef6e5a4e8f0e2240deb61e022
daniel23github <129347245+daniel23github@users.noreply.github.com>
2025-10-07 12:56:25 -0300

    implementa how-to-play

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import "./styles/main.scss";
+import "./styles/main-menu.scss";
+import "./styles/how-to-play.scss";
+
+const mainMenu = document.getElementById("main-menu")
+const howToPlayButton = document.getElementById("show-how-to-play");
+const howToPlay = document.getElementById("how-to-play");
+const x = document.getElementById("x");
+
+howToPlayButton?.addEventListener("click", () => {
+    mainMenu?.classList.add("hidden");
+    howToPlay?.classList.remove("hidden");
+
+})
+
+x?.addEventListener("click", () => {
+    howToPlay?.classList.add("hidden");
+    mainMenu?.classList.remove("hidden");
+
+})
