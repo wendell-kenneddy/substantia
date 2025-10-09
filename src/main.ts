1e67f2bf9fd488332e15b71f4160c934fc13c214
daniel23github <129347245+daniel23github@users.noreply.github.com>
2025-10-08 22:34:06 -0300

    salvar antes do merge

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,17 @@ import "./styles/main.scss";
 import "./styles/main-menu.scss";
 import "./styles/how-to-play.scss";
 
-const mainMenu = document.getElementById("main-menu")
+const mainMenu = document.getElementById("main-menu");
 const howToPlayButton = document.getElementById("show-how-to-play");
 const howToPlay = document.getElementById("how-to-play");
 const x = document.getElementById("x");
 
 howToPlayButton?.addEventListener("click", () => {
-    mainMenu?.classList.add("hidden");
-    howToPlay?.classList.remove("hidden");
-
-})
+  mainMenu?.classList.add("hidden");
+  howToPlay?.classList.remove("hidden");
+});
 
 x?.addEventListener("click", () => {
-    howToPlay?.classList.add("hidden");
-    mainMenu?.classList.remove("hidden");
-
-})
+  howToPlay?.classList.add("hidden");
+  mainMenu?.classList.remove("hidden");
+});
